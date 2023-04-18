6b73167ae233aadfdbbc6457e313134feba5c232
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-04-18 09:15:54 +0300

    Add files via upload

---
+++ задача10.py
@@ -0,0 +1,15 @@
+N = int(input('Введите количество монет '))
+orel = reshka = 0
+for i in range(N):
+    x = int(input('Орел(1) или решка(0)? '))
+    if x == 1:
+        orel += 1
+    else:
+        reshka += 1
+if orel < reshka:
+    print(f'Переверните {orel} монет с орла на решку, их меньше всего')
+elif orel == reshka:
+    print(f'Количество орлов и решек одинаково, по {orel} штук')
+else:
+    print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))
+    
---
+++ задача14.py
@@ -0,0 +1,13 @@
+N = abs(int(input('Введите число N ')))
+stop = 0
+P = 2
+for i in range(N):
+    if stop != 1:
+        P = P ** i
+        if P <= N:
+            print(P, end=' ')
+            P = 2
+        else:
+            stop = 1
+    else:
+        i = N
